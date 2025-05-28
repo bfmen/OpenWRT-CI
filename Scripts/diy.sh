4b4abeac03513d9def6e0827a854fe13b6ef6aeb
ysuolmai <fan0417@gmail.com>
2025-05-28 18:10:33 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -328,9 +328,9 @@ if [ -d "package/vlmcsd" ]; then
     fi
 fi
 
-#if [ -d "package/luci-app-vlmcsd" ]; then
-#    find package/luci-app-vlmcsd -type f \( -name '*.js' -o -name '*.lua' -o -name '*.htm' \) -exec sed -i 's#/etc/vlmcsd.ini#/etc/vlmcsd/vlmcsd.ini#g' {} +
-#fi
+if [ -d "package/luci-app-vlmcsd" ]; then
+    find package/luci-app-vlmcsd -type f \( -name '*.js' -o -name '*.lua' -o -name '*.htm' \) -exec sed -i 's#/etc/vlmcsd.ini#/etc/vlmcsd/vlmcsd.ini#g' {} +
+fi
 
 
 #update golang
