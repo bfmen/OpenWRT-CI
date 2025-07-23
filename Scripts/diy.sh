d3d52fa2761906b528747f0b8d6f9630a4a9df45
ysuolmai <fan0417@gmail.com>
2025-07-23 22:55:42 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -277,6 +277,9 @@ sed -i '/define Package\/default-settings\/install/a \
 \t$(INSTALL_DIR) $(1)/etc\n\t$(INSTALL_DATA) ./files/99-distfeeds.conf $(1)/etc/99-distfeeds.conf' \
 package/emortal/default-settings/Makefile
 
+#解决 dropbear 配置的 bug
+install -Dm755 "${GITHUB_WORKSPACE}/Scripts/99_dropbear_setup.sh" "package/base-files/files/etc/uci-defaults/99_dropbear_setup"
+
 if [[ "$WRT_CONFIG" == *"EMMC"*]]; then
     #解决 nginx 的问题
     install -Dm755 "${GITHUB_WORKSPACE}/Scripts/99_nginx_setup.sh" "package/base-files/files/etc/uci-defaults/99_nginx_setup"
