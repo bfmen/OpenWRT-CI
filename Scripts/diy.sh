ba4bba2d7673f0095408b95c3db79278f254ae62
ysuolmai <fan0417@gmail.com>
2025-11-10 11:31:37 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -419,5 +419,6 @@ fix_openwrt_apk_versions() {
 fix_openwrt_apk_versions package
 
 #fix cmake minimum version issue
-sed -i '/CMAKE_SHARED_LINKER_FLAGS/a CMAKE_OPTIONS += -DCMAKE_POLICY_VERSION_MINIMUM=3.5' include/cmake.mk
-
+if ! grep -q "CMAKE_POLICY_VERSION_MINIMUM" include/cmake.mk; then
+  echo 'CMAKE_OPTIONS += -DCMAKE_POLICY_VERSION_MINIMUM=3.5' >> include/cmake.mk
+fi
